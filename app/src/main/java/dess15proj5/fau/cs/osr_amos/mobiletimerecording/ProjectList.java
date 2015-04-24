d9b176e9883a8e0be31db4cbafdcf15678372d14
Tobias Fertig <Tobias.Fertig@t-online.de>
2015-04-24 17:27:12 +0200

    updated ProjectList.java: it's now possible to create multiple projects with individual timers;

--- app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/ProjectList.java
+++ app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/ProjectList.java
@@ -1,21 +1,21 @@
 package dess15proj5.fau.cs.osr_amos.mobiletimerecording;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.SystemClock;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Chronometer;
-
+import android.widget.*;
+import dess15proj5.fau.cs.osr_amos.mobiletimerecording.utility.ProjectTimer;
 
 public class ProjectList extends ActionBarActivity
 {
-    private Chronometer chronometer;
-    private Button startButton;
-    private boolean chronometerRunning = false;
-    private long lastBreak = 0L;
+	private TableLayout tableLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,44 +23,8 @@ public class ProjectList extends ActionBarActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_list);
 
-        chronometer = (Chronometer) findViewById(R.id.chronometer);
-        startButton = ((Button) findViewById(R.id.start_button));
-		initializeStartButton();
+		tableLayout = (TableLayout)findViewById(R.id.project_list);
     }
-
-	/**
-	 * @methodtype command method
-	 */
-	private void initializeStartButton()
-	{
-		startButton.setOnClickListener(new View.OnClickListener()
-		{
-			@Override
-			public void onClick(View v)
-			{
-				if(!chronometerRunning)
-				{
-					if(lastBreak == 0L)
-					{
-						chronometer.setBase(SystemClock.elapsedRealtime());
-					} else
-					{
-						chronometer.setBase(chronometer.getBase() + SystemClock.elapsedRealtime() - lastBreak);
-					}
-					chronometer.start();
-					chronometerRunning = true;
-					startButton.setText("Stop");
-				} else
-				{
-					chronometer.stop();
-					lastBreak = SystemClock.elapsedRealtime();
-					chronometerRunning = false;
-					startButton.setText("Start");
-				}
-
-			}
-		});
-	}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -76,11 +40,71 @@ public class ProjectList extends ActionBarActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+		switch (item.getItemId())
+		{
+			case R.id.action_add_project:
+				addNewProject();
+				return true;
+			case R.id.action_settings:
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
     }
+
+	private void addNewProject()
+	{
+		final EditText input = new EditText(this);
+		new AlertDialog.Builder(ProjectList.this).setTitle("Update Status")
+												 .setMessage("hi")
+												 .setView(input)
+												 .setPositiveButton("Ok", new DialogInterface.OnClickListener()
+												 {
+													 public void onClick(DialogInterface dialog, int whichButton)
+													 {
+														 createTableRow(input);
+													 }
+												 })
+												 .setNegativeButton("Cancel", new DialogInterface.OnClickListener()
+												 {
+													 public void onClick(DialogInterface dialog, int whichButton)
+													 {
+														 // Do nothing.
+													 }
+												 })
+												 .show();
+	}
+
+	private void createTableRow(EditText editText)
+	{
+		TableRow row = (TableRow)LayoutInflater.from(ProjectList.this)
+											   .inflate(R.layout.project_row, null);
+		((TextView)row.findViewById(R.id.projectName)).setText(editText.getText());
+		initializeStartButton((Button)row.findViewById(R.id.startButton), (ProjectTimer)row.findViewById(R.id.timer));
+		tableLayout.addView(row);
+		tableLayout.requestLayout();
+	}
+
+	/**
+	 * @methodtype command method
+	 */
+	private void initializeStartButton(final Button button, final ProjectTimer timer)
+	{
+		button.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				if(!timer.isRunning())
+				{
+					timer.start();
+					button.setText("Stop");
+				} else
+				{
+					timer.stop();
+					button.setText("Start");
+				}
+			}
+		});
+	}
 }
