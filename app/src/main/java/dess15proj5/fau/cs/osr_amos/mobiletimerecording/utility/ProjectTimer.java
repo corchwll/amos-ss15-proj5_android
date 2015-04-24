4408399e8bc291a7728eed4ff09e7b4fe4218a89
Tobias Fertig <Tobias.Fertig@t-online.de>
2015-04-24 17:08:05 +0200

    added ProjectTimer.java: default android chronometer doesn't have a method to check whether it is running or not.

---
+++ app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/utility/ProjectTimer.java
@@ -0,0 +1,45 @@
+package dess15proj5.fau.cs.osr_amos.mobiletimerecording.utility;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Chronometer;
+
+
+public class ProjectTimer extends Chronometer
+{
+	private boolean isRunning = false;
+
+	public ProjectTimer(Context context)
+	{
+		super(context);
+	}
+
+	public ProjectTimer(Context context, AttributeSet attrs)
+	{
+		super(context, attrs);
+	}
+
+	public ProjectTimer(Context context, AttributeSet attrs, int defStyle)
+	{
+		super(context, attrs, defStyle);
+	}
+
+	@Override
+	public void start()
+	{
+		super.start();
+		isRunning = true;
+	}
+
+	@Override
+	public void stop()
+	{
+		super.stop();
+		isRunning = false;
+	}
+
+	public boolean isRunning()
+	{
+		return isRunning;
+	}
+}
