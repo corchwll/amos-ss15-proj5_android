a465a2589a94c08aba440856625d1cf05bac79e4
Tobias Fertig <Tobias.Fertig@t-online.de>
2015-04-24 15:38:17 +0200

    added PersistenceHelper.java

---
+++ app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/persistence/PersistenceHelper.java
@@ -0,0 +1,54 @@
+package dess15proj5.fau.cs.osr_amos.mobiletimerecording.persistence;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class PersistenceHelper extends SQLiteOpenHelper
+{
+	public static final String TABLE_PROJECTS = "projects";
+	public static final String PROJECTS_ID = "id";
+	public static final String PROJECTS_NAME = "name";
+
+	private static final String CREATE_PROJECTS =
+			"create table " + TABLE_PROJECTS + "(" + PROJECTS_ID + " integer primary key autoincrement, " +
+					PROJECTS_NAME + " text);";
+
+	public static final String TABLE_SESSIONS = "sessions";
+	public static final String SESSIONS_ID = "id";
+	public static final String SESSIONS_PROJECT_ID = "project_id";
+	public static final String SESSIONS_TIMESTAMP_START = "timestamp_start";
+	public static final String SESSIONS_TIMESTAMP_STOP = "timestamp_stop";
+
+	private static final String CREATE_SESSIONS =
+			"create table " + TABLE_SESSIONS + "(" + SESSIONS_ID + " integer primary key autoincrement, " +
+					SESSIONS_PROJECT_ID + " integer, " + SESSIONS_TIMESTAMP_START + " timestamp not null, " +
+					SESSIONS_TIMESTAMP_STOP + " timestamp, " + "foreign key(" + SESSIONS_PROJECT_ID + ") references " +
+					TABLE_PROJECTS + "(" + PROJECTS_ID + "));";
+
+	private static final String DATABASE_NAME = "mobile_time_recording";
+	private static final int DATABASE_VERSION = 1;
+
+	public PersistenceHelper(Context context)
+	{
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db)
+	{
+		db.execSQL(CREATE_PROJECTS);
+		db.execSQL(CREATE_SESSIONS);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+	{
+		Log.w(PersistenceHelper.class.getName(), "Upgrading database from version " + oldVersion + " to " + newVersion +
+						", which will destroy all old data");
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_SESSIONS);
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_PROJECTS);
+		onCreate(db);
+	}
+}
