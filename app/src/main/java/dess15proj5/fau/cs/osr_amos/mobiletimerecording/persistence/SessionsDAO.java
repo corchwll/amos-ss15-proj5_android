c9e152ad729c26fcc1f2469a59ac591865a069e9
Tobias Fertig <Tobias.Fertig@t-online.de>
2015-04-25 15:47:07 +0200

    added SessionsDAO; SessionsDAOImpl for persistency management;

---
+++ app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/persistence/SessionsDAO.java
@@ -0,0 +1,15 @@
+package dess15proj5.fau.cs.osr_amos.mobiletimerecording.persistence;
+
+import dess15proj5.fau.cs.osr_amos.mobiletimerecording.models.Session;
+
+import java.util.Date;
+import java.util.List;
+
+public interface SessionsDAO extends DAO
+{
+	public Session create(long projectId, Date startTime);
+	public void update(Session session);
+	public Session load(long sessionId);
+	public void delete(long sessionId);
+	public List<Session> listAll();
+}
---
+++ app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/persistence/SessionsDAOImpl.java
@@ -0,0 +1,115 @@
+package dess15proj5.fau.cs.osr_amos.mobiletimerecording.persistence;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import dess15proj5.fau.cs.osr_amos.mobiletimerecording.models.Session;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class SessionsDAOImpl implements SessionsDAO
+{
+	private SQLiteDatabase database;
+	private PersistenceHelper persistenceHelper;
+	private String[] allColumns = {PersistenceHelper.SESSIONS_ID, PersistenceHelper.SESSIONS_PROJECT_ID,
+			PersistenceHelper.SESSIONS_TIMESTAMP_START, PersistenceHelper.SESSIONS_TIMESTAMP_STOP};
+
+	public SessionsDAOImpl(Context context)
+	{
+		persistenceHelper = new PersistenceHelper(context);
+	}
+
+	@Override
+	public void open() throws SQLException
+	{
+		database = persistenceHelper.getWritableDatabase();
+	}
+
+	@Override
+	public void close()
+	{
+		persistenceHelper.close();
+	}
+
+	@Override
+	public Session create(long projectId, Date startTime)
+	{
+		ContentValues values = new ContentValues();
+		values.put(PersistenceHelper.SESSIONS_PROJECT_ID, projectId);
+		values.put(PersistenceHelper.SESSIONS_TIMESTAMP_START, startTime.getTime());
+		values.put(PersistenceHelper.SESSIONS_TIMESTAMP_STOP, startTime.getTime());
+		long insertId = database.insert(PersistenceHelper.TABLE_SESSIONS, null, values);
+
+		Cursor cursor = database.query(PersistenceHelper.TABLE_SESSIONS, allColumns, PersistenceHelper.SESSIONS_ID +
+				" = " + insertId, null, null, null, null);
+		cursor.moveToFirst();
+		Session newSession = cursorToSession(cursor);
+		cursor.close();
+		return newSession;
+	}
+
+	@Override
+	public void update(Session session)
+	{
+		ContentValues values = new ContentValues();
+		values.put(PersistenceHelper.SESSIONS_ID, session.getId());
+		values.put(PersistenceHelper.SESSIONS_PROJECT_ID, session.getProjectId());
+		values.put(PersistenceHelper.SESSIONS_TIMESTAMP_START, session.getStartTime()
+																	  .getTime());
+		values.put(PersistenceHelper.SESSIONS_TIMESTAMP_STOP, session.getStopTime()
+																	 .getTime());
+
+		database.update(PersistenceHelper.TABLE_SESSIONS, values,
+				PersistenceHelper.SESSIONS_ID + " = " + session.getId(), null);
+	}
+
+	@Override
+	public Session load(long sessionId)
+	{
+		Cursor cursor = database.query(PersistenceHelper.TABLE_SESSIONS, allColumns, PersistenceHelper.SESSIONS_ID +
+				" = " + sessionId, null, null, null, null);
+		cursor.moveToFirst();
+		Session session = cursorToSession(cursor);
+		cursor.close();
+		return session;
+	}
+
+	@Override
+	public void delete(long sessionId)
+	{
+		database.delete(PersistenceHelper.TABLE_SESSIONS, PersistenceHelper.SESSIONS_ID + " = " + sessionId, null);
+	}
+
+	@Override
+	public List<Session> listAll()
+	{
+		List<Session> sessions= new ArrayList<>();
+
+		Cursor cursor = database.query(PersistenceHelper.TABLE_PROJECTS, allColumns, null, null, null, null, null);
+
+		cursor.moveToFirst();
+		while(!cursor.isAfterLast())
+		{
+			Session session = cursorToSession(cursor);
+			sessions.add(session);
+			cursor.moveToNext();
+		}
+
+		cursor.close();
+		return sessions;
+	}
+
+	private Session cursorToSession(Cursor cursor)
+	{
+		Session session = new Session();
+		session.setId(cursor.getLong(0));
+		session.setProjectId(cursor.getLong(1));
+		session.setStartTime(new Date(cursor.getLong(2)));
+		session.setStopTime(new Date(cursor.getLong(3)));
+		return session;
+	}
+}
