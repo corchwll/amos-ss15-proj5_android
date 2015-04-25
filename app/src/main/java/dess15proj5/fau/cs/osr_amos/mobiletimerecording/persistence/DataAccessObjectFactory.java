daaa31aedd571736c0e07e2d00c0b7a73c1f774d
Tobias Fertig <Tobias.Fertig@t-online.de>
2015-04-25 15:47:36 +0200

    added DataAccessObjectFactory.java; SQLiteDataAccessObjectFactory.java;

---
+++ app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/persistence/DataAccessObjectFactory.java
@@ -0,0 +1,28 @@
+package dess15proj5.fau.cs.osr_amos.mobiletimerecording.persistence;
+
+import android.content.Context;
+
+public abstract class DataAccessObjectFactory
+{
+	private static DataAccessObjectFactory instance = new SQLiteDataAccessObjectFactory();
+
+	public static DataAccessObjectFactory getInstance()
+	{
+		return instance;
+	}
+
+	public static void setInstance(DataAccessObjectFactory dAOFactory)
+	{
+		instance = dAOFactory;
+	}
+
+	public ProjectsDAO createProjectsDAO(Context context)
+	{
+		return instance.createProjectsDAO(context);
+	}
+
+	public SessionsDAO createSessionsDAO(Context context)
+	{
+		return instance.createSessionsDAO(context);
+	}
+}
---
+++ app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/persistence/SQLiteDataAccessObjectFactory.java
@@ -0,0 +1,30 @@
+package dess15proj5.fau.cs.osr_amos.mobiletimerecording.persistence;
+
+import android.content.Context;
+
+public class SQLiteDataAccessObjectFactory extends DataAccessObjectFactory
+{
+	private static DataAccessObjectFactory instance;
+
+	public static DataAccessObjectFactory getInstance()
+	{
+		if(instance == null)
+		{
+			instance = new SQLiteDataAccessObjectFactory();
+		}
+
+		return instance;
+	}
+
+	@Override
+	public ProjectsDAO createProjectsDAO(Context context)
+	{
+		return new ProjectsDAOImpl(context);
+	}
+
+	@Override
+	public SessionsDAO createSessionsDAO(Context context)
+	{
+		return new SessionsDAOImpl(context);
+	}
+}
