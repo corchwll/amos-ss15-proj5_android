1ad67e078505590198a87710672494bce9b74365
Tobias Fertig <Tobias.Fertig@t-online.de>
2015-04-25 17:32:01 +0200

    updated ProjectList.java: new projects are now saved persistent and loaded on startup;

--- app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/ProjectList.java
+++ app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/ProjectList.java
@@ -1,8 +1,10 @@
 package dess15proj5.fau.cs.osr_amos.mobiletimerecording;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.SystemClock;
+import android.provider.ContactsContract;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -11,36 +13,44 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.*;
+import dess15proj5.fau.cs.osr_amos.mobiletimerecording.models.Project;
+import dess15proj5.fau.cs.osr_amos.mobiletimerecording.persistence.DataAccessObjectFactory;
+import dess15proj5.fau.cs.osr_amos.mobiletimerecording.persistence.ProjectsDAO;
 import dess15proj5.fau.cs.osr_amos.mobiletimerecording.utility.ProjectTimer;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class ProjectList extends ActionBarActivity
 {
 	private TableLayout tableLayout;
+	private final Context context = this;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
 	{
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_project_list);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_project_list);
 
 		tableLayout = (TableLayout)findViewById(R.id.project_list);
-    }
+		loadProjectList();
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_project_list, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_project_list, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-		switch (item.getItemId())
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		switch(item.getItemId())
 		{
 			case R.id.action_add_project:
 				addNewProject();
@@ -50,7 +60,7 @@ public class ProjectList extends ActionBarActivity
 			default:
 				return super.onOptionsItemSelected(item);
 		}
-    }
+	}
 
 	private void addNewProject()
 	{
@@ -62,7 +72,17 @@ public class ProjectList extends ActionBarActivity
 												 {
 													 public void onClick(DialogInterface dialog, int whichButton)
 													 {
-														 createTableRow(input);
+														 try
+														 {
+															 Project project = createNewProject(input.getText()
+																									 .toString());
+															 createTableRow(project);
+														 } catch(SQLException e)
+														 {
+															 Toast.makeText(context, "Could not create new project " +
+																	 "due to database errors!", Toast.LENGTH_LONG);
+														 }
+
 													 }
 												 })
 												 .setNegativeButton("Cancel", new DialogInterface.OnClickListener()
@@ -75,11 +95,50 @@ public class ProjectList extends ActionBarActivity
 												 .show();
 	}
 
-	private void createTableRow(EditText editText)
+	private Project createNewProject(String projectName) throws SQLException
+	{
+		ProjectsDAO projectsDAO = DataAccessObjectFactory.getInstance()
+														 .createProjectsDAO(context);
+
+		projectsDAO.open();
+		Project project = projectsDAO.create(projectName);
+		projectsDAO.close();
+
+		return project;
+	}
+
+	private void loadProjectList()
+	{
+		try
+		{
+			List<Project> projects = loadAllProjects();
+			for(Project project : projects)
+			{
+				createTableRow(project);
+			}
+		} catch(SQLException e)
+		{
+			Toast.makeText(context, "Could not load project list due to database errors!", Toast.LENGTH_LONG);
+		}
+	}
+
+	private List<Project> loadAllProjects() throws SQLException
+	{
+		ProjectsDAO projectsDAO = DataAccessObjectFactory.getInstance()
+														 .createProjectsDAO(context);
+
+		projectsDAO.open();
+		List<Project> projects = projectsDAO.listAll();
+		projectsDAO.close();
+
+		return projects;
+	}
+
+	private void createTableRow(Project project)
 	{
 		TableRow row = (TableRow)LayoutInflater.from(ProjectList.this)
 											   .inflate(R.layout.project_row, null);
-		((TextView)row.findViewById(R.id.projectName)).setText(editText.getText());
+		((TextView)row.findViewById(R.id.projectName)).setText(project.getName());
 		initializeStartButton((Button)row.findViewById(R.id.startButton), (ProjectTimer)row.findViewById(R.id.timer));
 		tableLayout.addView(row);
 		tableLayout.requestLayout();
