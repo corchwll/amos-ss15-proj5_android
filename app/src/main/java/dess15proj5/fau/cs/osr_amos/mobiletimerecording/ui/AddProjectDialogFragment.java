890ccff02b28668d9effbc72fd05a929cfa3589f
Tobias Fertig <Tobias.Fertig@t-online.de>
2015-04-30 12:06:58 +0200

    reformated AddProjectDialogFragment.java; deleted redundant ProjectList.java;

--- app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/ui/AddProjectDialogFragment.java
+++ app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/ui/AddProjectDialogFragment.java
@@ -24,44 +24,50 @@ public class AddProjectDialogFragment extends DialogFragment
 	}
 
 	AddProjectDialogListener callbackListener;
-    private View view;
+	private View view;
 
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState)
-    {
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-        view = inflater.inflate(R.layout.add_project, null);
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setView(view)
-                .setTitle("Please add new project")
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        try {
-                            EditText newProjectId = (EditText)view.findViewById(R.id.newProjectId);
-                            Long newProjectIdAsLong = Long.parseLong(newProjectId.getText()
-																				 .toString());
-                            EditText newProjectName = (EditText) view.findViewById(R.id.newProjectName);
-                            String newProjectNameAsString = newProjectName.getText().toString();
-                            createNewProject(newProjectIdAsLong, newProjectNameAsString);
-							callbackListener.onDialogPositiveClick(AddProjectDialogFragment.this);
-                        } catch (SQLException e) {
-                            Toast.makeText(getActivity(), "Could not create new project " +
-                                    "due to database errors!", Toast.LENGTH_LONG)
-                                    .show();
-                        }
-                    }
-                })
-                .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                            }
-                        }
-                );
-                return builder.create();
-                }
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState)
+	{
+		LayoutInflater inflater = getActivity().getLayoutInflater();
+		view = inflater.inflate(R.layout.add_project, null);
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		builder.setView(view)
+			   .setTitle("Please add new project")
+			   .setPositiveButton("OK", new DialogInterface.OnClickListener()
+			   {
+				   @Override
+				   public void onClick(DialogInterface dialog, int which)
+				   {
+					   try
+					   {
+						   EditText newProjectId = (EditText)view.findViewById(R.id.newProjectId);
+						   Long newProjectIdAsLong = Long.parseLong(newProjectId.getText()
+																				.toString());
+						   EditText newProjectName = (EditText)view.findViewById(R.id.newProjectName);
+						   String newProjectNameAsString = newProjectName.getText()
+																		 .toString();
+						   createNewProject(newProjectIdAsLong, newProjectNameAsString);
+						   callbackListener.onDialogPositiveClick(AddProjectDialogFragment.this);
+					   } catch(SQLException e)
+					   {
+						   Toast.makeText(getActivity(), "Could not create new project " + "due to database errors!",
+								   Toast.LENGTH_LONG)
+								.show();
+					   }
+				   }
+			   })
+			   .setNegativeButton("CANCEL", new DialogInterface.OnClickListener()
+					   {
+						   @Override
+						   public void onClick(DialogInterface dialog, int which)
+						   {
+						   }
+					   });
+		return builder.create();
+	}
 
-    private void createNewProject(long projectId, String projectName) throws SQLException
+	private void createNewProject(long projectId, String projectName) throws SQLException
 	{
 		ProjectsDAO projectsDAO = DataAccessObjectFactory.getInstance()
 														 .createProjectsDAO(getActivity());
@@ -77,7 +83,7 @@ public class AddProjectDialogFragment extends DialogFragment
 		super.onAttach(activity);
 		try
 		{
-			callbackListener = (AddProjectDialogListener) activity;
+			callbackListener = (AddProjectDialogListener)activity;
 		} catch(ClassCastException e)
 		{
 			throw new ClassCastException(activity.toString() + " must implement AddProjectDialogListener");
