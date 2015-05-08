feabc015320bb2ecddecacf8f827d93f823ece02
ChristianHa <christian.happ@fau.de>
2015-05-08 16:36:03 +0200

    added ui/layout to change entries in user profile

---
+++ app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/ui/ChangeUserProfileActivity.java
@@ -0,0 +1,15 @@
+package dess15proj5.fau.cs.osr_amos.mobiletimerecording.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import dess15proj5.fau.cs.osr_amos.mobiletimerecording.R;
+
+public class ChangeUserProfileActivity extends ActionBarActivity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.change_user_profile);
+	}
+}
