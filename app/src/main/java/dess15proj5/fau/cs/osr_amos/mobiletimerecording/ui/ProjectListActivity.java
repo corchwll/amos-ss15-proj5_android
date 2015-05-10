6687372c0b61ac7536ca7b543be790cef67d1f5f
ChristianHa <christian.happ@fau.de>
2015-05-10 11:25:45 +0200

    added NavigationDrawer (forget adding files)

--- app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/ui/ProjectListActivity.java
+++ app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/ui/ProjectListActivity.java
@@ -3,9 +3,14 @@ package dess15proj5.fau.cs.osr_amos.mobiletimerecording.ui;
 import android.app.DialogFragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 import dess15proj5.fau.cs.osr_amos.mobiletimerecording.R;
@@ -19,16 +24,65 @@ import java.util.List;
 public class ProjectListActivity extends ActionBarActivity implements AddProjectDialogFragment.AddProjectDialogListener
 {
 	private ProjectArrayAdapter adapter;
+	private ActionBarDrawerToggle actionBarDrawerToggle;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_project_list);
+
+		setAdapterToProjectList();
+
+		setAdapterToNavigationDrawer();
+
+		loadProjectList();
+	}
+
+	private void setAdapterToProjectList()
+	{
 		adapter = new ProjectArrayAdapter(this);
 		ListView projectList = (ListView) findViewById(R.id.project_list);
 		projectList.setAdapter(adapter);
-		loadProjectList();
+	}
+
+	private void setAdapterToNavigationDrawer()
+	{
+		String[] drawerListItems = getResources().getStringArray(R.array.drawer_list_items);
+		DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.navigation_drawer);
+		final ListView leftDrawer = (ListView) findViewById(R.id.left_drawer);
+		leftDrawer.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, drawerListItems));
+		leftDrawer.setOnItemClickListener(new AdapterView.OnItemClickListener()
+		{
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+			{
+				switch(position)
+				{
+					case 1:
+						Intent intent = new Intent(getBaseContext(), ChangeUserProfileActivity.class);
+						startActivity(intent);
+				}
+			}
+		});
+		actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+				 R.string.drawer_open, R.string.drawer_close)
+		{
+			@Override
+			public void onDrawerClosed(View drawerView)
+			{
+				super.onDrawerClosed(drawerView);
+			}
+
+			@Override
+			public void onDrawerOpened(View drawerView)
+			{
+				super.onDrawerOpened(drawerView);
+			}
+		};
+		drawerLayout.setDrawerListener(actionBarDrawerToggle);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 	}
 
 	@Override
@@ -41,14 +95,14 @@ public class ProjectListActivity extends ActionBarActivity implements AddProject
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item)
 	{
+		if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+          return true;
+        }
+
 		switch(item.getItemId())
 		{
 			case R.id.action_add_project:
 				addNewProject();
-				return true;
-			case R.id.action_change_user_profile:
-				Intent intent = new Intent(this, ChangeUserProfileActivity.class);
-				startActivity(intent);
 				return true;
 			case R.id.action_settings:
 				return true;
