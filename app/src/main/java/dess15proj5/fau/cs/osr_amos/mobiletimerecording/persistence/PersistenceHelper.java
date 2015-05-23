f5536ae9a28c5b45d215025ca55390b324e8e0db
Tobias Fertig <Tobias.Fertig@t-online.de>
2015-05-23 13:30:55 +0200

    updated PersistenceHelper.java: added inline documentation

--- app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/persistence/PersistenceHelper.java
+++ app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/persistence/PersistenceHelper.java
@@ -29,6 +29,11 @@ import java.util.List;
 
 public class PersistenceHelper extends SQLiteOpenHelper
 {
+	public static List<String> defaultProjects = new ArrayList<>();
+
+	/*
+	 * Constants for the database table projects
+	 */
 	public static final String TABLE_PROJECTS = "projects";
 	public static final String PROJECTS_ID = "id";
 	public static final String PROJECTS_NAME = "name";
@@ -37,19 +42,26 @@ public class PersistenceHelper extends SQLiteOpenHelper
 	public static final String PROJECTS_IS_USED = "is_used";
 	public static final String PROJECTS_IS_ARCHIVED = "is_archived";
 
+	/*
+	 * Create Table statement for the table projects
+	 */
 	private static final String CREATE_PROJECTS =
 			"create table " + TABLE_PROJECTS + "(" + PROJECTS_ID + " text primary key not null, " +
 					PROJECTS_NAME + " text, " + PROJECTS_FINAL_DATE + " integer, " + PROJECTS_IS_DISPLAYED +
 					" integer, " + PROJECTS_IS_USED + " integer, " + PROJECTS_IS_ARCHIVED + " integer);";
 
-	public static List<String> defaultProjects = new ArrayList<>();
-
+	/*
+	 * Constants for the database table sessions
+	 */
 	public static final String TABLE_SESSIONS = "sessions";
 	public static final String SESSIONS_ID = "id";
 	public static final String SESSIONS_PROJECT_ID = "project_id";
 	public static final String SESSIONS_TIMESTAMP_START = "start_time";
 	public static final String SESSIONS_TIMESTAMP_STOP = "stop_time";
 
+	/*
+	 * Create Table statement for the table sessions
+	 */
 	private static final String CREATE_SESSIONS =
 			"create table " + TABLE_SESSIONS + "(" + SESSIONS_ID + " integer primary key autoincrement, " +
 					SESSIONS_PROJECT_ID + " text, " + SESSIONS_TIMESTAMP_START + " integer not null, " +
@@ -57,6 +69,9 @@ public class PersistenceHelper extends SQLiteOpenHelper
 					"references " +
 					TABLE_PROJECTS + "(" + PROJECTS_ID + "));";
 
+	/*
+	 * Constants for the database table users
+	 */
 	public static final String TABLE_USERS = "users";
 	public static final String USERS_ID = "id";
 	public static final String USERS_LAST_NAME = "last_name";
@@ -67,20 +82,38 @@ public class PersistenceHelper extends SQLiteOpenHelper
 	public static final String USERS_CURRENT_OVERTIME = "current_overtime";
 	public static final String USERS_REGISTRATION_DATE = "registration_date";
 
+	/*
+	 * Create Table statement for the table users
+	 */
 	private static final String CREATE_USERS =
 			"create table " + TABLE_USERS + "(" + USERS_ID + " text primary key not null, " +
 					USERS_LAST_NAME + " text, " + USERS_FIRST_NAME + " text, " + USERS_WEEKLY_WORKING_TIME + " " +
 					"integer, " + USERS_TOTAL_VACATION_TIME + " integer, " + USERS_CURRENT_VACATION_TIME + " integer," +
 					USERS_CURRENT_OVERTIME + " integer, " + USERS_REGISTRATION_DATE + " integer);";
 
+	/*
+	 * Constants for database name and version
+	 */
 	private static final String DATABASE_NAME = "mobile_time_recording";
 	private static final int DATABASE_VERSION = 7;
 
+	/**
+	 * Constructor for the PersistenceHelper.
+	 *
+	 * @param context the application context under which this object should be constructed.
+	 * @methodtype constructor
+	 */
 	public PersistenceHelper(Context context)
 	{
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
 	}
 
+	/**
+	 * This method is called in the android lifecycle when the database is used for the first time.
+	 *
+	 * @param db the database object provided by the android application.
+	 * @methodtype initialization method
+	 */
 	@Override
 	public void onCreate(SQLiteDatabase db)
 	{
@@ -90,8 +123,15 @@ public class PersistenceHelper extends SQLiteOpenHelper
 		db.execSQL(CREATE_USERS);
 	}
 
+	/**
+	 * This method is internally used to insert the default projects in to the created database.
+	 *
+	 * @param db the database object provided by the android application.
+	 * @methodtype command method
+	 */
 	private void insertDefaultProjects(SQLiteDatabase db)
 	{
+		//insertion of default project Vacation
 		ContentValues values = new ContentValues();
 		values.put(PROJECTS_ID, "10000");
 		values.put(PROJECTS_NAME, "Vacation");
@@ -101,6 +141,7 @@ public class PersistenceHelper extends SQLiteOpenHelper
 		values.put(PROJECTS_IS_ARCHIVED, 0);
 		db.insert(TABLE_PROJECTS, null, values);
 
+		//insertion of default project Training
 		values = new ContentValues();
 		values.put(PROJECTS_ID, "10001");
 		values.put(PROJECTS_NAME, "Training");
@@ -110,6 +151,7 @@ public class PersistenceHelper extends SQLiteOpenHelper
 		values.put(PROJECTS_IS_ARCHIVED, 0);
 		db.insert(TABLE_PROJECTS, null, values);
 
+		//insertion of default project Illness
 		values = new ContentValues();
 		values.put(PROJECTS_ID, "10002");
 		values.put(PROJECTS_NAME, "Illness");
@@ -119,6 +161,7 @@ public class PersistenceHelper extends SQLiteOpenHelper
 		values.put(PROJECTS_IS_ARCHIVED, 0);
 		db.insert(TABLE_PROJECTS, null, values);
 
+		//insertion of default project Office
 		values = new ContentValues();
 		values.put(PROJECTS_ID, "10003");
 		values.put(PROJECTS_NAME, "Office");
@@ -128,12 +171,21 @@ public class PersistenceHelper extends SQLiteOpenHelper
 		values.put(PROJECTS_IS_ARCHIVED, 0);
 		db.insert(TABLE_PROJECTS, null, values);
 
+		//keeping track of the default project ids
 		defaultProjects.add("10000");
 		defaultProjects.add("10001");
 		defaultProjects.add("10002");
 		defaultProjects.add("10003");
 	}
 
+	/**
+	 * This method is called in the android lifecycle if the version of the database has changed.
+	 *
+	 * @param db the database object provided by the android application
+	 * @param oldVersion the version number of the old database
+	 * @param newVersion the version number of the new database
+	 * @methodtype command method
+	 */
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
 	{
