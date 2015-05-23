10ffd1169766c467c7ae8bade11c43b970cb8662
Tobias Fertig <Tobias.Fertig@t-online.de>
2015-05-23 13:13:27 +0200

    updated DataAccessObjectFactory.java: improved coding style and added inline documentation

--- app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/persistence/DataAccessObjectFactory.java
+++ app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/persistence/DataAccessObjectFactory.java
@@ -24,21 +24,48 @@ import java.sql.SQLException;
 
 public abstract class DataAccessObjectFactory
 {
-	private static DataAccessObjectFactory instance = new SQLiteDataAccessObjectFactory();
+	private static DataAccessObjectFactory instance;
 	private ProjectsDAO projectsDAO;
 	private SessionsDAO sessionsDAO;
 	private UsersDAO usersDAO;
 
+	/**
+	 * This method returns an instance of DataAccessObjectFactory to fulfill the singleton pattern. If there wasn't
+	 * initialized an instance yet, it will be constructed.
+	 *
+	 * @return the singleton instance of the DataAccessObjectFactory
+	 * @methodtype get method
+	 */
 	public static DataAccessObjectFactory getInstance()
 	{
+		if(instance == null)
+		{
+			instance = new SQLiteDataAccessObjectFactory();
+		}
+
 		return instance;
 	}
 
+	/**
+	 * This method allows to inject another DataAccessObjectFactory if needed.
+	 *
+	 * @param dAOFactory DataAccessObjectFactory
+	 * @methodtype set method
+	 */
 	public static void setInstance(DataAccessObjectFactory dAOFactory)
 	{
 		instance = dAOFactory;
 	}
 
+	/**
+	 * This method creates a DAO for projects if there wasn't already one created. It also opens the database
+	 * connection so that queries can instantly be executed.
+	 *
+	 * @param context The application context under which the database object will be created
+	 * @return The requested ProjectsDAO will be returned
+	 * @throws SQLException
+	 * @methodtype factory method
+	 */
 	public ProjectsDAO createProjectsDAO(Context context) throws SQLException
 	{
 		if(projectsDAO == null)
@@ -46,11 +73,29 @@ public abstract class DataAccessObjectFactory
 			projectsDAO = doCreateProjectsDAO(context);
 			projectsDAO.open();
 		}
+
 		return projectsDAO;
 	}
 
+	/**
+	 * This is the abstract method an actual factory class can overwrite to specify the concrete DAO object which
+	 * should be used.
+	 *
+	 * @param context The application context under which the database object will be created
+	 * @return The concrete ProjectsDAO will be returned
+	 * @methodtype initialization method
+	 */
 	protected abstract ProjectsDAO doCreateProjectsDAO(Context context);
 
+	/**
+	 * This method creates a DAO for sessions if there wasn't already one created. It also opens the database
+	 * connection so that queries can instantly be executed.
+	 *
+	 * @param context The application context under which the database object will be created
+	 * @return The requested SessionsDAO will be returned
+	 * @throws SQLException
+	 * @methodtype factory method
+	 */
 	public SessionsDAO createSessionsDAO(Context context) throws SQLException
 	{
 		if(sessionsDAO == null)
@@ -58,11 +103,29 @@ public abstract class DataAccessObjectFactory
 			sessionsDAO = doCreateSessionsDAO(context);
 			sessionsDAO.open();
 		}
+
 		return sessionsDAO;
 	}
 
+	/**
+	 * This is the abstract method an actual factory class can overwrite to specify the concrete DAO object which
+	 * should be used.
+	 *
+	 * @param context The application context under which the database object will be created
+	 * @return The concrete SessionsDAO will be returned
+	 * @methodtype initialization method
+	 */
 	protected abstract SessionsDAO doCreateSessionsDAO(Context context);
 
+	/**
+	 * This method creates a DAO for users if there wasn't already one created. It also opens the database
+	 * connection so that queries can instantly be executed.
+	 *
+	 * @param context The application context under which the database object will be created
+	 * @return The requested UsersDAO will be returned
+	 * @throws SQLException
+	 * @methodtype factory method
+	 */
 	public UsersDAO createUsersDAO(Context context) throws SQLException
 	{
 		if(usersDAO == null)
@@ -70,8 +133,17 @@ public abstract class DataAccessObjectFactory
 			usersDAO = doCreateUsersDAO(context);
 			usersDAO.open();
 		}
+
 		return usersDAO;
 	}
 
+	/**
+	 * This is the abstract method an actual factory class can overwrite to specify the concrete DAO object which
+	 * should be used.
+	 *
+	 * @param context The application context under which the database object will be created
+	 * @return The concrete UsersDAO will be returned
+	 * @methodtype initialization method
+	 */
 	protected abstract UsersDAO doCreateUsersDAO(Context context);
 }
