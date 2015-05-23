4d05ddf0b6a26a7853a2afd137072c481332fbc4
Tobias Fertig <Tobias.Fertig@t-online.de>
2015-05-23 17:11:07 +0200

    updated RegisterUserProfileFragment.java: added inline documentation

--- app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/ui/RegisterUserProfileFragment.java
+++ app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/ui/RegisterUserProfileFragment.java
@@ -30,6 +30,14 @@ import java.util.Date;
 
 public class RegisterUserProfileFragment extends AbstractUserProfileFragment
 {
+	/**
+	 * This method is called in the android lifecycle when the view of the fragment is created.
+	 *
+	 * @param inflater this param contains the layout inflater which is used to generate the gui
+	 * @param container the container is used by the layout inflater
+	 * @param savedInstanceState this param contains several key value pairs in order to save the instance state
+	 * methodtype initialization method
+	 */
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
@@ -38,6 +46,12 @@ public class RegisterUserProfileFragment extends AbstractUserProfileFragment
 		return scrollView;
 	}
 
+	/**
+	 * This method is used in the android lifecycle when the option menu is prepared.
+	 *
+	 * @param menu the menu that is prepared
+	 * methodtype initialization method
+	 */
 	@Override
 	public void onPrepareOptionsMenu(Menu menu)
 	{
@@ -45,6 +59,18 @@ public class RegisterUserProfileFragment extends AbstractUserProfileFragment
 		menu.getItem(0).setVisible(false);
 	}
 
+	/**
+	 * This method is used to store an user object to the database.
+	 *
+	 * @param employeeId the id of the user that has to be stored
+	 * @param lastName the last name of the user that has to be stored
+	 * @param firstName the first name of the user that has to be stored
+	 * @param weeklyWorkingTime the hours a user has to work per week
+	 * @param totalVacationTime the amount of days a user can take off per year
+	 * @param currentVacationTime the amount of days a user has left to take off this year
+	 * @param currentOvertime the overtime the user actual has
+	 * methodtype command method
+	 */
 	@Override
 	protected void runDBTransaction(String employeeId, String lastName, String firstName, int weeklyWorkingTime,
 									int totalVacationTime, int currentVacationTime, int currentOvertime)
