3fa2db170429e97a6ea71986594a59a8c341eed5
Tobias Fertig <Tobias.Fertig@t-online.de>
2015-05-23 14:10:17 +0200

    updated SQLiteDataAccessObjectFactory.java: added inline documentation

--- app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/persistence/SQLiteDataAccessObjectFactory.java
+++ app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/persistence/SQLiteDataAccessObjectFactory.java
@@ -24,6 +24,13 @@ public class SQLiteDataAccessObjectFactory extends DataAccessObjectFactory
 {
 	private static DataAccessObjectFactory instance;
 
+	/**
+	 * This method returns an instance of DataAccessObjectFactory to fulfill the singleton pattern. If there wasn't
+	 * initialized an instance yet, it will be constructed.
+	 *
+	 * @return the singleton instance of the DataAccessObjectFactory
+	 * @methodtype get method
+	 */
 	public static DataAccessObjectFactory getInstance()
 	{
 		if(instance == null)
@@ -34,18 +41,39 @@ public class SQLiteDataAccessObjectFactory extends DataAccessObjectFactory
 		return instance;
 	}
 
+	/**
+	 * This method specifies the concrete DAO object which should be used.
+	 *
+	 * @param context The application context under which the database object will be created
+	 * @return The concrete ProjectsDAO will be returned
+	 * @methodtype initialization method
+	 */
 	@Override
 	public ProjectsDAO doCreateProjectsDAO(Context context)
 	{
 		return new ProjectsDAOImpl(context);
 	}
 
+	/**
+	 * This method specifies the concrete DAO object which should be used.
+	 *
+	 * @param context The application context under which the database object will be created
+	 * @return The concrete SessionsDAO will be returned
+	 * @methodtype initialization method
+	 */
 	@Override
 	public SessionsDAO doCreateSessionsDAO(Context context)
 	{
 		return new SessionsDAOImpl(context);
 	}
 
+	/**
+	 * This method specifies the concrete DAO object which should be used.
+	 *
+	 * @param context The application context under which the database object will be created
+	 * @return The concrete UsersDAO will be returned
+	 * @methodtype initialization method
+	 */
 	@Override
 	protected UsersDAO doCreateUsersDAO(Context context)
 	{
