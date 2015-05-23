63899a6c6b30bb80ab4170f91adc0accf5a1c762
Tobias Fertig <Tobias.Fertig@t-online.de>
2015-05-23 14:02:11 +0200

    updated SessionsDAO.java: added inline documentation

--- app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/persistence/SessionsDAO.java
+++ app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/persistence/SessionsDAO.java
@@ -25,11 +25,66 @@ import java.util.List;
 
 public interface SessionsDAO extends DAO
 {
+	/**
+	 * This method inserts the given information into the sessions table and creates an object of type session.
+	 *
+	 * @param projectId the project id the required session belongs to
+	 * @param startTime the time when the new session started
+	 * @return the required session object is returned
+	 * @methodtype conversion method (since the given information is converted into an object of type session)
+	 */
 	Session create(String projectId, Date startTime);
+
+	/**
+	 * This method inserts the given information into the sessions table and creates an object of type session.
+	 *
+	 * @param projectId the project id the required session belongs to
+	 * @param startTime the time when the new session started
+	 * @param stopTime the time when the new session terminated
+	 * @return the required session object is returned
+	 * @methodtype conversion method (since the given information is converted into an object of type session)
+	 */
 	Session create(String projectId, Date startTime, Date stopTime);
+
+	/**
+	 * This method is used to update a given session in the database.
+	 *
+	 * @param session the session which has to be updated.
+	 * @methodtype command method
+	 */
 	void update(Session session);
+
+	/**
+	 * This method loads the session with the given id from the database.
+	 *
+	 * @param sessionId the id of the project that should be loaded from database
+	 * @return the session matching the given id
+	 * @methodtype query method
+	 */
 	Session load(long sessionId);
+
+	/**
+	 * This method deletes the session with the given id from the database.
+	 *
+	 * @param sessionId the id of the project that should be deleted
+	 * @methodtype command method
+	 */
 	void delete(long sessionId);
+
+	/**
+	 * This method loads all sessions from the database that are belonging to the given projectId.
+	 *
+	 * @param projectId the id the sessions have to belong to
+	 * @return a list containing all sessions for the given projectId
+	 * @methodtype query method
+	 */
 	List<Session> listAllForProject(String projectId);
+
+	/**
+	 * This method loads all sessions from the database.
+	 *
+	 * @return a list containing all sessions
+	 * @methodtype query method
+	 */
 	List<Session> listAll();
 }
