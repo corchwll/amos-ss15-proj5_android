3ce9bdca727adae3b0ff44013cbff6712e17ffd1
Tobias Fertig <Tobias.Fertig@t-online.de>
2015-06-21 16:30:18 +0200

    added inline documentation to GPSPoint.java

--- app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/models/GPSPoint.java
+++ app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/models/GPSPoint.java
@@ -23,6 +23,13 @@ public class GPSPoint
 	private double latitude;
 	private double longitude;
 
+	/**
+	 * This method is used to construct an object of type GPSPoint.
+	 *
+	 * @param latitude the latitude value for the point
+	 * @param longitude the longitude value for the point
+	 * methodtype constructor
+	 */
 	public GPSPoint(double latitude, double longitude)
 	{
 		assert latitude >= -90.0 && latitude <= 90.0 : "latitude has to be between -90.0 and 90.0";
@@ -31,22 +38,50 @@ public class GPSPoint
 		this.longitude = longitude;
 	}
 
+	/**
+	 * This method returns the value of the latitude attribute.
+	 *
+	 * @return the double value of the latitude
+	 * methodtype get method
+	 */
 	public double getLatitude()
 	{
 		return latitude;
 	}
 
+	/**
+	 * This method sets the value of the latitude attribute if valid value is used.
+	 *
+	 * @param latitude the value for the latitude attribute
+	 * methodtype set method
+	 * pre latitude >= -90.0 && latitude <= 90.0
+	 * post latitude value successfully set
+	 */
 	public void setLatitude(double latitude)
 	{
 		assert latitude >= -90.0 && latitude <= 90.0 : "latitude has to be between -90.0 and 90.0";
 		this.latitude = latitude;
 	}
 
+	/**
+	 * This method returns the value of the longitude attribute.
+	 *
+	 * @return the double value of the longitude
+	 * methodtype get method
+	 */
 	public double getLongitude()
 	{
 		return longitude;
 	}
 
+	/**
+	 * This method sets the value of the longitude attribute if valid value is used.
+	 *
+	 * @param longitude the value for the longitude attribute
+	 * methodtype set method
+	 * pre longitude >= -180.0 && longitude <= 180.0
+	 * post longitude value successfully set
+	 */
 	public void setLongitude(double longitude)
 	{
 		assert longitude >= -180.0 && latitude <= 180.0 : "latitude has to be between -180.0 and 180.0";
