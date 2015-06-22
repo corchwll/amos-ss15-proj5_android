ce2e425493186d0ae77f659e9507d118109a6eba
Tobias Fertig <Tobias.Fertig@t-online.de>
2015-06-22 11:23:20 +0200

    #18 added class to create notification for android

---
+++ app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/businesslogic/AccountingNotification.java
@@ -0,0 +1,60 @@
+/*
+ *     Mobile Time Accounting
+ *     Copyright (C) 2015
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dess15proj5.fau.cs.osr_amos.mobiletimerecording.businesslogic;
+
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import dess15proj5.fau.cs.osr_amos.mobiletimerecording.R;
+import dess15proj5.fau.cs.osr_amos.mobiletimerecording.ui.MainActivity;
+
+public class AccountingNotification
+{
+	private Context context;
+
+	public AccountingNotification(Context context)
+	{
+		this.context = context;
+	}
+
+	public void createNotification()
+	{
+		Notification.Builder mBuilder =
+				new Notification.Builder(context)
+						.setSmallIcon(R.mipmap.ic_launcher)
+						.setContentTitle("Mobile Time Accounting")
+						.setContentText("Sie haben für heute noch keine Zeiten aufgenommen!");
+		// Prepare intent which is triggered if the
+		// notification is selected
+		Intent intent = new Intent(context, MainActivity.class);
+		PendingIntent pIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+		// Build notification
+		// Actions are just fake
+
+		mBuilder.setContentIntent(pIntent);
+		NotificationManager mNotificationManager =
+				(NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		// mId allows you to update the notification later on.
+		mNotificationManager.notify(50, mBuilder.build());
+	}
+}
