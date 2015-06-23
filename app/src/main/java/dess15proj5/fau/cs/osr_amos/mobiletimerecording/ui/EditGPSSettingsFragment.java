0ded41ef3014c111db0b1d409c56a8a98dfb6fa0
Tobias Fertig <Tobias.Fertig@t-online.de>
2015-06-23 16:17:42 +0200

    added new activity to edit the gps settings

--- app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/ui/EditGPSSettingsFragment.java
+++ app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/ui/EditGPSSettingsFragment.java
@@ -1,5 +1,116 @@
+/*
+ *     Mobile Time Accounting
+ *     Copyright (C) 2015
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package dess15proj5.fau.cs.osr_amos.mobiletimerecording.ui;
 
-public class EditGPSSettingsFragment
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CheckBox;
+import dess15proj5.fau.cs.osr_amos.mobiletimerecording.R;
+
+public class EditGPSSettingsFragment extends AppCompatActivity
 {
+	protected CheckBox checkboxWidget;
+
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.gps_settings);
+		checkboxWidget = (CheckBox)findViewById(R.id.useGPS);
+		loadPreferences();
+
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+		if(getSupportActionBar() != null)
+		{
+			getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+			getSupportActionBar().setHomeButtonEnabled(true);
+		}
+	}
+
+	public void loadPreferences()
+	{
+		SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+		checkboxWidget.setChecked(sharedPref.getBoolean("useGPS", false));
+	}
+
+	/**
+	 * This method is called in the android lifecycle when a menu item is clicked on.
+	 *
+	 * @param item the item which was targeted
+	 * @return true if there was an item clicked
+	 * methodtype boolean query method
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch(item.getItemId())
+		{
+			case android.R.id.home:
+				onBackPressed();
+				break;
+			case 1:
+				onSavePressed();
+				break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	public void onSavePressed()
+	{
+		SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+		SharedPreferences.Editor editor = sharedPref.edit();
+		editor.putBoolean("useGPS", checkboxWidget.isChecked());
+		editor.apply();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	/**
+	 *  Called when the activity has detected the user's press of the back key.
+	 *
+	 * methodtype command method
+	 */
+	@Override
+	public void onBackPressed()
+	{
+			finishActivityAndShowAnimation();
+	}
+
+	/**
+	 *  Finish the activity and shows an animation
+	 *
+	 * methodtype command method
+	 */
+	private void finishActivityAndShowAnimation()
+	{
+		super.finish();
+		overridePendingTransition(R.animator.empty_animator, R.animator.fade_out_right);
+	}
 }
