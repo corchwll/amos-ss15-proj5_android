55d33f71c1278c41ec5af186077446d5a71f8532
Tobias Fertig <Tobias.Fertig@t-online.de>
2015-06-23 10:36:04 +0200

    #23 fixed a bug in GPSPoint

--- app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/models/GPSPoint.java
+++ app/src/main/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/models/GPSPoint.java
@@ -84,7 +84,7 @@ public class GPSPoint
 	 */
 	public void setLongitude(double longitude)
 	{
-		assert longitude >= -180.0 && latitude <= 180.0 : "latitude has to be between -180.0 and 180.0";
+		assert longitude >= -180.0 && longitude <= 180.0 : "latitude has to be between -180.0 and 180.0";
 		this.longitude = longitude;
 	}
 }
