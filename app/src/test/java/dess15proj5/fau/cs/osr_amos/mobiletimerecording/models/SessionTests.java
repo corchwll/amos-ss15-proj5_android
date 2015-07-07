28c07c8d959c7f632ebde0ce5018e042e33e713c
Tobias Fertig <Tobias.Fertig@t-online.de>
2015-07-07 11:33:23 +0200

    #74 added test classes for User and Session model classes; wrote all test case signatures

---
+++ app/src/test/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/models/SessionTests.java
@@ -0,0 +1,96 @@
+/*
+ *     Mobile Time Accounting
+ *     Copyright (C) 2015
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dess15proj5.fau.cs.osr_amos.mobiletimerecording.models;
+
+import org.junit.Test;
+
+public class SessionTests
+{
+	@Test
+	public void testGetId_MethodCalled_IDReturned()
+	{
+
+	}
+
+	@Test
+	public void testSetId_IDGreaterZero_CorrectSet()
+	{
+
+	}
+
+	@Test
+	public void testSetId_IDLowerZero_AssertionThrown()
+	{
+
+	}
+
+	@Test
+	public void testGetProjectId_MethodCalled_ProjectIDReturned()
+	{
+
+	}
+
+	@Test
+	public void testSetProjectId_IDGreaterZero_CorrectSet()
+	{
+
+	}
+
+	@Test
+	public void testSetProjectId_IDLowerZero_AssertionThrown()
+	{
+
+	}
+
+	@Test
+	public void testGetStartTime_MethodCalled_StartTimeReturned()
+	{
+
+	}
+
+	@Test
+	public void testSetStartTime_CorrectStartTime_CorrectSet()
+	{
+
+	}
+
+	@Test
+	public void testSetStartTime_NullParameter_AssertionThrown()
+	{
+
+	}
+
+	@Test
+	public void testGetStopTime_MethodCalled_CorrectSet()
+	{
+
+	}
+
+	@Test
+	public void testSetStopTime_CorrectStopTime_CorrectSet()
+	{
+
+	}
+
+	@Test
+	public void testSetStopTime_NullParameter_AssertionThrown()
+	{
+
+	}
+}
---
+++ app/src/test/java/dess15proj5/fau/cs/osr_amos/mobiletimerecording/models/UserTests.java
@@ -0,0 +1,162 @@
+/*
+ *     Mobile Time Accounting
+ *     Copyright (C) 2015
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dess15proj5.fau.cs.osr_amos.mobiletimerecording.models;
+
+import org.junit.Test;
+
+public class UserTests
+{
+	@Test
+	public void testGetEmployeeId_MethodCalled_EmployeeIDReturned()
+	{
+
+	}
+
+	@Test
+	public void testSetEmployeeId_LengthEqualsFive_CorrectSet()
+	{
+
+	}
+
+	@Test
+	public void testSetEmployeeId_LengthNotEqualsFive_AssertionThrown()
+	{
+
+	}
+
+	@Test
+	public void testGetLastName_MethodCalled_LastNameReturned()
+	{
+
+	}
+
+	@Test
+	public void testSetLastName_CorrectValue_CorrectSet()
+	{
+
+	}
+
+	@Test
+	public void testSetLastName_NullValue_AssertionThrown()
+	{
+
+	}
+
+	@Test
+	public void testGetFirstName_MethodCalled_FirstNameReturned()
+	{
+
+	}
+
+	@Test
+	public void testSetFirstName_CorrectValue_CorrectSet()
+	{
+
+	}
+
+	@Test
+	public void testSetFirstName_NullValue_AssertionThrown()
+	{
+
+	}
+
+	@Test
+	public void testGetWeeklyWorkingTime_MethodCalled_WeeklyWorkingTimeReturned()
+	{
+
+	}
+
+	@Test
+	public void testSetWeeklyWorkingTime_TimeGreaterZero_CorrectSet()
+	{
+
+	}
+
+	@Test
+	public void testSetWeeklyWorkingTime_TimeLowerZero_AssertionThrown()
+	{
+
+	}
+
+	@Test
+	public void testGetTotalVacationTime_MethodCalled_TotalVacationTimeReturned()
+	{
+
+	}
+
+	@Test
+	public void testSetTotalVacationTime_TimeGreaterZero_CorrectSet()
+	{
+
+	}
+
+	@Test
+	public void testSetTotalVacationTime_TimeLowerZero_AssertionThrown()
+	{
+
+	}
+
+	@Test
+	public void testGetCurrentVacationTime_MethodCalled_CurrentVacationTimeReturned()
+	{
+
+	}
+
+	@Test
+	public void testSetCurrentVacationTime_TimeGreaterZero_CorrectSet()
+	{
+
+	}
+
+	@Test
+	public void testSetCurrentVacationTime_TimeLowerZero_AssertionThrown()
+	{
+
+	}
+
+	@Test
+	public void testGetCurrentOvertime_MethodCalled_CurrentOvertimeReturned()
+	{
+
+	}
+
+	@Test
+	public void testSetCurrentOvertime_MethodCalled_CorrectSet()
+	{
+
+	}
+
+	@Test
+	public void testGetRegistrationDate_MethodCalled_RegistrationDateReturned()
+	{
+
+	}
+
+	@Test
+	public void testSetRegistrationDate_CorrectValue_CorrectSet()
+	{
+
+	}
+
+	@Test
+	public void testSetRegistrationDate_NullValue_AssertionThrown()
+	{
+
+	}
+}
